09eeb57512f4eb7939b139f0a10c5daca24e8319
gayatri-l-rajput <rajputlalit591@gmail.com>
2023-04-02 19:36:31 +0530

    feat: added initial component

---
+++ src/mockData/mockData.ts
@@ -0,0 +1,275 @@
+export const data = {
+  primary_count: 48,
+  count: 48,
+  room_distribution: [
+    {
+      adults: '2',
+      children: [5, 0],
+    },
+  ],
+  map_bounding_box: {
+    sw_long: 73.7550133466721,
+    ne_lat: 15.569826348713,
+    ne_long: 73.780736,
+    sw_lat: 15.527074,
+  },
+  total_count_with_filters: 48,
+  unfiltered_count: 438,
+  extended_count: 0,
+  unfiltered_primary_count: 438,
+  search_radius: 0,
+  sort: [
+    {
+      id: 'distance',
+      name: 'Distance from city centre',
+    },
+    {
+      name: 'Popularity',
+      id: 'popularity',
+    },
+    {
+      name: 'Stars and other ratings (5 to 0)',
+      id: 'class_descending',
+    },
+    {
+      name: 'Stars and other ratings (0 to 5)\n',
+      id: 'class_ascending',
+    },
+    {
+      id: 'bayesian_review_score',
+      name: 'Guest review score',
+    },
+    {
+      id: 'price',
+      name: 'Price (low to high)',
+    },
+  ],
+  result: [
+    {
+      native_ad_id: '',
+      soldout: 0,
+      mobile_discount_percentage: 0,
+      cc1: 'in',
+      city: 'Baga',
+      hotel_id: 4575421,
+      hotel_has_vb_boost: 0,
+      district_id: 0,
+      unit_configuration_label:
+        '<b>Entire apartment – 117 m²</b>: 5 beds • 2 bedrooms • 1 living room • 2 bathrooms',
+      is_no_prepayment_block: 0,
+      longitude: 73.763933,
+      hotel_include_breakfast: 0,
+      cc_required: 1,
+      review_score: 8.9,
+      distance_to_cc: '1.45',
+      type: 'property_card',
+      review_score_word: 'Fabulous',
+      address: 'Calangute - Arpora Road The eternal wave probo waddo calangute',
+      updated_checkin: null,
+      preferred_plus: 1,
+      price_is_final: 1,
+      checkin: {
+        until: '23:30',
+        from: '13:00',
+      },
+      main_photo_id: 224463704,
+      main_photo_url:
+        'https://cf.bstatic.com/xdata/images/hotel/square60/224463704.jpg?k=d898a8fdbf8e69cbe6f6b9e4d9b8f4af3942432b0b441d2daf023c3181e349ca&o=',
+      native_ads_cpc: 0,
+      cant_book: 0,
+      block_ids: ['457542105_335356040_6_0_0'],
+      districts: '',
+      review_nr: 46,
+      matching_units_configuration: {
+        matching_units_common_config: {
+          localized_area: '117',
+          unit_type_id: 1,
+        },
+      },
+      accommodation_type: 201,
+      hotel_name: 'Aarju Luxurious Wave',
+      default_language: 'xu',
+      hotel_name_trans: 'Aarju Luxurious Wave',
+      distance_to_cc_formatted: '1.4 km',
+      zip: '403519',
+      timezone: 'Asia/Kolkata',
+      distance: '1.42',
+      is_mobile_deal: 0,
+      price_breakdown: {
+        has_incalculable_charges: 0,
+        has_tax_exceptions: 0,
+        gross_price: '7616.00',
+        has_fine_print_charges: 0,
+        currency: 'INR',
+        all_inclusive_price: 7616,
+        sum_excluded_raw: '0.00',
+      },
+      children_not_allowed: 0,
+      default_wishlist_name: 'Calangute',
+      distances: [
+        {
+          text: '1.4 km from centre',
+          icon_set: null,
+          icon_name: 'bui_geo_pin',
+        },
+        {
+          icon_name: 'bui_distance',
+          text: '1.2 km from beach',
+          icon_set: null,
+        },
+      ],
+      extended: 0,
+      currencycode: 'INR',
+      is_city_center: 0,
+      city_trans: 'Calangute',
+      accommodation_type_name: 'Apartment',
+      hotel_facilities:
+        '452,471,56,5,17,456,46,468,460,161,222,453,44,14,140,461,160,16,449,454,467,47,305,457,64,425,488,48,163,96,158,181,450,458,462,22,109,418,8,455,107,28,119,301,101,193,419,129,485,127,91,2,423,433,445,424,104,304,436,465',
+      id: 'property_card_4575421',
+      url: 'https://www.booking.com/hotel/in/aarju-luxurious-stay-wave.html',
+      review_recommendation: '',
+      in_best_district: 0,
+      class_is_estimated: 0,
+      currency_code: 'INR',
+      bwallet: {
+        hotel_eligibility: 0,
+      },
+      is_beach_front: 0,
+      district: '',
+      selected_review_topic: null,
+      min_total_price: 7615.99998176098,
+      checkout: {
+        from: '',
+        until: '10:00',
+      },
+      preferred: 1,
+      address_trans:
+        'Calangute - Arpora Road The eternal wave probo waddo calangute',
+      city_name_en: 'Baga',
+      is_genius_deal: 0,
+      badges: [],
+      ufi: -2093662,
+      latitude: 15.556296,
+      countrycode: 'in',
+      genius_discount_percentage: 0,
+      country_trans: 'India',
+      updated_checkout: null,
+      city_in_trans: 'in Calangute',
+      wishlist_count: 0,
+      is_free_cancellable: 1,
+      has_swimming_pool: 1,
+      class: 0,
+      booking_home: {
+        segment: 1,
+        is_single_unit_property: '',
+        is_booking_home: 1,
+        quality_class: 4,
+        group: 'apartment_like',
+      },
+      native_ads_tracking: '',
+      composite_price_breakdown: {
+        gross_amount_per_night: {
+          value: 7615.99998176098,
+          currency: 'INR',
+        },
+        excluded_amount: {
+          value: 0,
+          currency: 'INR',
+        },
+        all_inclusive_amount: {
+          currency: 'INR',
+          value: 7615.99998176098,
+        },
+        product_price_breakdowns: [
+          {
+            gross_amount_per_night: {
+              value: 7615.99998176098,
+              currency: 'INR',
+            },
+            all_inclusive_amount: {
+              value: 7615.99998176098,
+              currency: 'INR',
+            },
+            excluded_amount: {
+              value: 0,
+              currency: 'INR',
+            },
+            gross_amount_hotel_currency: {
+              currency: 'INR',
+              value: 7615.99998176098,
+            },
+            included_taxes_and_charges_amount: {
+              currency: 'INR',
+              value: 815.999981760979,
+            },
+            items: [
+              {
+                item_amount: {
+                  currency: 'INR',
+                  value: 815.999981760979,
+                },
+                base: {
+                  base_amount: 0,
+                  kind: 'variable_amount',
+                },
+                name: 'Goods & services tax',
+                details: 'Goods & services tax',
+                inclusion_type: 'included',
+                kind: 'charge',
+              },
+            ],
+            benefits: [],
+            net_amount: {
+              currency: 'INR',
+              value: 6800,
+            },
+            gross_amount: {
+              currency: 'INR',
+              value: 7615.99998176098,
+            },
+          },
+        ],
+        items: [
+          {
+            kind: 'charge',
+            details: 'Goods & services tax',
+            inclusion_type: 'included',
+            base: {
+              base_amount: 0,
+              kind: 'variable_amount',
+            },
+            name: 'Goods & services tax',
+            item_amount: {
+              currency: 'INR',
+              value: 815.999981760979,
+            },
+          },
+        ],
+        included_taxes_and_charges_amount: {
+          currency: 'INR',
+          value: 815.999981760979,
+        },
+        gross_amount_hotel_currency: {
+          currency: 'INR',
+          value: 7615.99998176098,
+        },
+        benefits: [],
+        net_amount: {
+          currency: 'INR',
+          value: 6800,
+        },
+        gross_amount: {
+          currency: 'INR',
+          value: 7615.99998176098,
+        },
+      },
+      has_free_parking: 1,
+      is_geo_rate: '',
+      is_smart_deal: 0,
+      max_photo_url:
+        'https://cf.bstatic.com/xdata/images/hotel/max1280x900/224463704.jpg?k=d898a8fdbf8e69cbe6f6b9e4d9b8f4af3942432b0b441d2daf023c3181e349ca&o=',
+      max_1440_photo_url:
+        'https://cf.bstatic.com/xdata/images/hotel/1440x1440/224463704.jpg?k=d898a8fdbf8e69cbe6f6b9e4d9b8f4af3942432b0b441d2daf023c3181e349ca&o=',
+    },
+  ],
+};
